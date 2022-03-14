b229003b53c2395e8a3448f65a8b0436d0f6f1af
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-03-14 14:28:35 +0100

    Add create_board

--- engine.py
+++ engine.py
@@ -1,3 +1,9 @@
+import random
+
+
+WALLS = ['░']
+
+
 def create_board(width, height):
     '''
     Creates a new game board based on input parameters.
@@ -9,7 +15,20 @@ def create_board(width, height):
     Returns:
     list: Game board
     '''
-    pass
+    board = []
+    board.append(['░' for i in range(width)])
+    for i in range(height - 2):
+        row = [' ' for i in range(width - 2)]
+        row.insert(0, '░')
+        row.append('░')
+        board.append(row)
+    board.append(['░' for i in range(width)])
+    board[random.randint(1, height-2)][-1] = 'B'
+    for row in board:
+        print(''.join(row))
+
+
+create_board(30, 20)
 
 
 def put_player_on_board(board, player):
