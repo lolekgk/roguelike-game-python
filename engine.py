25696b7a8fc2d91809d90e00523e6e0336869460
lolekgk <karol.gajda97@gmail.com>
2022-03-15 20:08:59 +0100

    Add put_on_board and is_put_valid  functions
    Added 3 functions, that put on board player, npc and functions and is_put_on_board_valid functions.

--- engine.py
+++ engine.py
@@ -1,9 +1,11 @@
-
 from util import key_pressed
 import random
 
 
 WALLS = ['░']
+PLAYER_ICON = '☺'
+PLAYER_START_X = 3
+PLAYER_START_Y = 3
 
 
 def create_board(width, height):
@@ -19,8 +21,8 @@ def create_board(width, height):
     return board
 
 
-
 def put_player_on_board(board, player):
+    board[PLAYER_START_X][PLAYER_START_Y] = PLAYER_ICON
     '''
     Modifies the game board by placing the player icon at its coordinates.
 
@@ -31,9 +33,32 @@ def put_player_on_board(board, player):
     Returns:
     Nothing
     '''
-    pass
 
-player = {"field":(2,2)}
+
+def put_npc_on_board(board, npc):
+    '''randomowe rozmieszczenie postaci npc'''
+    while True:
+        y = random.randint(0, len(board[0])-1)
+        x = random.randint(0, len(board)-1)
+        if board[x][y] == ' ':
+            board[x][y] = 'n'
+            break 
+
+
+def put_items_on_board(board, item):
+    '''randomowe rozmieszczenie przedmiotow'''
+    while True:
+        y = random.randint(0, len(board[0])-1)
+        x = random.randint(0, len(board)-1)
+        if board[x][y] == ' ':
+            board[x][y] = 'i'
+            break 
+
+
+def is_put_on_board_valid(board, x, y):
+    if board[x][y] == ' ':
+        return True
+    return False
 
 
 def is_move_valid(board, new_row, new_column):
@@ -60,3 +85,19 @@ def player_move(board, key):
 
 def npc_move():
     pass
+
+
+
+board = create_board(30, 20)
+put_player_on_board(board, 1)
+
+def display_board(board):
+    for row in board:
+        print(''.join(row))
+
+# player = {"field":(2,2)}
+put_npc_on_board(board, 1)
+put_items_on_board(board, 1)
+display_board(board)      
+
+
