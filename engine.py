47e209dc35baf57101b3d3059344cdece0f6feaa
Karolina Chalupka <k.s.chalupka@gmail.com>
2022-03-17 12:08:27 +0100

    chyba nic nie zmienilam

--- engine.py
+++ engine.py
@@ -1,6 +1,6 @@
 from items_and_characters import ITEMS
 import random
-import copy
+
 
 WALLS = ['░']
 ROW = 0
@@ -72,8 +72,7 @@ def is_move_valid(board, new_row, new_column):
     return True
 
 
-def move(character, board, key, player, items):
-    (row, column) = character["field"]
+def get_new_coords(row, column, key):
     if key == "W":
         new_row, new_column = row - 1, column
     elif key == "A":
@@ -84,30 +83,41 @@ def move(character, board, key, player, items):
         new_row, new_column = row, column + 1
     else:
         new_row, new_column = row, column
+    return new_row, new_column
+
+
+def move(character, board, key):
+    (row, column) = character["field"]
+    new_row, new_column = get_new_coords(row, column, key)
     if is_move_valid(board, new_row, new_column):
-        if board[new_row][new_column] in ICONS:
-            interaction_with_item(board, player, items, new_row, new_column)
         board[row][column] = " "
         character["field"] = (new_row, new_column)
         board[new_row][new_column] = character["icon"]
        
 
-def get_item(board, row, col, items):
-    if board[row][col] in ICONS:
+def is_interaction_with_item(board, player):
+    if board[player["field"][0]][player["field"][1]] in ICONS:
+        return True
+    return False
+
+
+def get_item(board, coords, items):
+    if board[coords[0]][coords[1]] in ICONS:
         for item in items:
-            if board[row][col] == item["icon"]:
+            if board[coords[0]][coords[1]] == item["icon"]:
                 return item
 
 
-def interaction_with_item(board, player, items, row, col):
-    item = get_item(board, row, col, items)
-    item["total amount"] -= 1
-    player["energy"] += item["effect"]["energy"]
-    player["knowledge"] += item["effect"]["knowledge"]
+def interaction_with_item(board, player, items):
+    if is_interaction_with_item(board, player):
+        item = get_item(board, player["field"], items)
+        item["total amount"] -= 1
+        player["energy"] += item["effect"]["energy"]
+        player["knowledge"] += item["effect"]["knowledge"]
          
 
 def is_interaction_with_npc(player, board):
-    (row, column) = player["field"]
+    (row, column) = player["position"]
     if board(row -1, column) in NPC_ICONS or board(row +1, column) in NPC_ICONS \
         or board(row, column - 1) in NPC_ICONS or board(row, column + 1) in NPC_ICONS:
         return True
