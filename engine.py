43cf30d57732c6c98aac88a7334db18e50feff04
Karolina Chalupka <k.s.chalupka@gmail.com>
2022-03-27 22:31:13 +0200

    working version

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 from items_and_characters import ITEMS
 import random
+import ui
 
 
 PLAYER_WALLS = ['░', "♥", "‼"]
@@ -8,6 +9,7 @@ EMPTY = " "
 ROW = 0
 ICONS = [item["icon"] for item in ITEMS]
 NPC_ICONS = ["♥", "‼"]
+PLAYER_DATA_TO_DISPLAY = ["name", "class", 'knowledge', 'smartness', 'energy', 'exams']
 
 
 def create_board(width, height):
@@ -25,7 +27,7 @@ def create_board(width, height):
 
 def create_stats_scroll(player, height) -> list:
     stats_scroll = ['  _______________________', '=(__    ___      __     _)=', '  |                     |']
-    for k in player:
+    for k in PLAYER_DATA_TO_DISPLAY:
         row = f'  | {k}: {player[k]}'
         while len(row) != len(' _______________________'):
             row += EMPTY
@@ -87,13 +89,15 @@ def get_new_coords(row, column, key):
     return new_row, new_column
 
 
-def move(character, board, key, player, items):
+def move(character, board, key, player, items, npcs):
     row, column = character["field"]
     new_row, new_column = get_new_coords(row, column, key)
     obstacles = PLAYER_WALLS if character == player else NPC_WALLS
     if is_move_valid(board, new_row, new_column, obstacles):
         if board[new_row][new_column] in ICONS and character == player:
             interaction_with_item(board, player, items, new_row, new_column)
+        elif is_interaction_with_npc(player, board):
+            interaction_with_npc(board, player, npcs)
         board[row][column] = EMPTY
         character["field"] = (new_row, new_column)
         board[new_row][new_column] = character["icon"]
@@ -115,42 +119,55 @@ def interaction_with_item(board, player, items, row, col):
 def update_player(player, item):
     player["energy"] += item["effect"]["energy"]
     player["knowledge"] += item["effect"]["knowledge"]
+    if item["name"] in player["inventory"]:
+        player["inventory"]["name"] += 1
 
     
 def is_interaction_with_npc(player, board):
-    row, column = player["position"]
-    if board(row - 1, column) in NPC_ICONS or board(row + 1, column) in NPC_ICONS \
-        or board(row, column - 1) in NPC_ICONS or board(row, column + 1) in NPC_ICONS:
+    row, column = player["field"]
+    if board[row - 1][column] in NPC_ICONS or board[row + 1][column] in NPC_ICONS \
+        or board[row][column - 1] in NPC_ICONS or board[row][column + 1] in NPC_ICONS:
         return True
     return False
 
 
 def get_npc(player, npcs):
-    row, column = player["position"]
-    adjacent_fields = [(row - 1, column), (row + 1, column), (row, column - 1), (row, column + 1)]
+    row, column = player["field"]
+    adjacent_fields = [(row + 1, column), (row, column + 1), (row - 1, column),  (row, column - 1), ]
     for npc in npcs:
-        if npc["position"] in adjacent_fields:
+        if npc["field"] in adjacent_fields:
             return npc
 
 
-def will_player_succeed(player, npc):
+def will_player_succeed(player, npc, weapon):
     smartness = player["smartness"]
-    wheapon = 2
     basic_prob = npc["attribute"][1]
-    success_prob = min(1, (smartness + wheapon) * 0.25 + basic_prob)
-    failure_prob = max(0, 1 - (smartness + wheapon) * 0.25 + basic_prob)
+    weapon_amount = weapon[1]
+    success_prob = min(1, (smartness + weapon_amount) * 0.25 + basic_prob)
+    failure_prob = max(0, 1 - (smartness + weapon_amount) * 0.25 + basic_prob)
     result = [True, False]
     weights = [success_prob, failure_prob]
     return random.choices(result, weights)
-    
+
+
+def find_item_by_name(name):
+    for item in ITEMS:
+        if item["name"] == name:
+            return item
 
 def interaction_with_npc(board, player, npcs):
-    if is_interaction_with_npc(player):
+    if is_interaction_with_npc(player, board):
         npc = get_npc(player, npcs)
         # print some dialog window
-        if will_player_succeed:
-            update_player(player, npc["attribute"][0])
-            row, column = npc["position"]
-            board[row][column] = EMPTY
+        # player choose wheapon
+        weapon = ui.choose_weapon(player)
+        if will_player_succeed(player, npc, weapon):
+            name = npc["attribute"][0]
+            item = find_item_by_name(name)
+            update_player(player, item)
+            row, column = npc["field"] 
+            board[row][column] = EMPTY 
+            npcs.remove(npc)
+        player["energy"] += npc["energy damage"]
 
         
--- items_and_characters.py
+++ items_and_characters.py
@@ -1,12 +1,12 @@
 
 #the value for "effect" is a tuple containing info about which studen's attribute is affected and how
 ITEMS = [
-        {"name": "notes",               "icon": "█", "total amount": 4, "effect": {'energy': 0, "knowledge": 3} },
-        {"name": "nerd's notes",        "icon": "◘", "total amount": 0, "effect": {'energy': -2, "knowledge": 3} },
-        {"name": "last year's test",    "icon": "T", "total amount": 0, "effect": {'energy': -4, "knowledge": 6} },
+        {"name": "notes",               "icon": "█", "total amount": 4, "effect": {"energy": 0, "knowledge": 1} },
         {"name": "Red Bull",            "icon": "E", "total amount": 8, "effect": {"energy": 4, "knowledge": 1} },
         {"name": "instant noodles",     "icon": "§", "total amount": 6, "effect": {"energy": 2, 'knowledge': 0} },
-        {"name": "beer",                "icon": "%", "total amount": 6, "effect": {"energy": 0, "knowledge": 0} }
+        {"name": "beer",                "icon": "%", "total amount": 6, "effect": {"energy": 0, "knowledge": 0} },
+        {"name": "nerd's notes",        "icon": "◘", "total amount": 0, "effect": {'energy': 1, "knowledge": 3} },
+        {"name": "last year's test",    "icon": "T", "total amount": 0, "effect": {'energy': 2, "knowledge": 6} }
         ]
 
 
@@ -17,18 +17,18 @@ COMPLEX_ITEM = {"name": "fridge", "icon": "[]", "total amount": 1, "items_list":
 
   
 # the value for "attributes" is a list of tuples; 
-# each tuple cointains item name and probability of getting that item upon interaction
-# probabilieties sum up to 1
+# each tuple cointains item name and probability of getting that item upon interaction of player with smartness 0 without any wheapon in inventory
+# the probability increase with smartness and "wheapon"
 NPCS = [ 
-        {"name": "best student",       "icon": "♥", "field": ( 3, 2), "attribute": ("nerd's notes", 0.5)},
-        {"name": "perpetual student",  "icon": "‼", "field": (18,14), "attribute": ("last year's test", 0)}
+        {"name": "best student",       "icon": "♥", "field": ( 3, 2), "attribute": ("nerd's notes", 0.25), "energy damage": -3},
+        {"name": "perpetual student",  "icon": "‼", "field": (18,14), "attribute": ("last year's test", 0), "energy damage": -5}
         ]
 
 
 # a dictionary of dictionaries with type name as key
 # the value is a dictionary with a character's parameters
 PLAYER_TYPES = {
-        'Nerd':         {'class': 'Nerd',       'knowledge': 10, 'smartness': 0, 'energy': 20, 'exams': 0}, 
-        'Average':      {'class': 'Average',    'knowledge': 5,  'smartness': 1, 'energy': 20, 'exams': 0},
-        'Laid-back':    {'class': 'Laid-back',  'knowledge': 1,  'smartness': 2, 'energy': 20, 'exams': 0}
+        'Nerd':         {'class': 'Nerd',       'knowledge': 10, 'smartness': 0, 'energy': 20, 'exams': 0, "inventory": {"beer": 0}}, 
+        'Average':      {'class': 'Average',    'knowledge': 5,  'smartness': 1, 'energy': 20, 'exams': 0, "inventory": {"beer": 0}},
+        'Laid-back':    {'class': 'Laid-back',  'knowledge': 1,  'smartness': 2, 'energy': 20, 'exams': 0, "inventory": {"beer": 0}}
         }
