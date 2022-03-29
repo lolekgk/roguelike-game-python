9774c777f57b5c96085dc0a02e1d8929588e05d1
lolekgk <karol.gajda97@gmail.com>
2022-03-29 13:57:18 +0200

    Add class with colours

--- ui.py
+++ ui.py
@@ -5,6 +5,17 @@ import time
 
 from items_and_characters import ITEMS, PLAYER_TYPES
 
+class bcolors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[96m'
+
 
 TYPES = ['Nerd', 'Laid-back', 'Average'] # = [k for k in PLAYER_TYPES.keys()] 
 
