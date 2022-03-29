90146e223a65a539c56be20969fc02758b80ca33
Sebastian Dudkowski <sebastian.dudkowski@gmail.com>
2022-03-29 10:56:00 +0200

    copy-paste main.py from development

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-from items_and_characters import PLAYER_TYPES, BOSS
+from items_and_characters import PLAYER_TYPES
 import util
 import engine
 import ui
 from items_and_characters import ITEMS, NPCS, PLAYER_TYPES
 from copy import deepcopy
 import random
+import gamesaves
+import time
 
 
 BOARD_WIDTH = 30
 BOARD_HEIGHT = 20
 PLAYER_ICON = "☻"
 PLAYER_START_COORDS = (1,1)
+LEVELS = [1, 2, 3]
+
+intro_level1 = ["You are a young, more or less brilliant student",
+                "who is about to finish their first year of studies.",
+                "You have spend the whole year studying / partying",
+                "and enjoying the student's life.",
+                "Unfortunately all good things come to an end...",
+                "Ahead of you is the most dreadful period for any student...",
+                "THE EXAMS!" ,
+                "Your first task is to prepare for them the best you can.",
+                "Increase your knowledge or use less honorable ways",
+                "to increase your chances of passing all final exams."]
+
+intro_level2 = ["You have done the best you can",
+                "to prepare yourself for the final exams",
+                "in the little time you had.",
+                "Your next challange is to defeat the 3 professors",
+                "and actually pass the exams",
+                "This task won't be easy, having good knowledge helps",
+                "but as all students know there are other ways",
+                "to get what you need from the noble members of Academia..."]
+
+intro_level3 = ["Congratulations!",
+                "You have passed the final exams and you are getting",
+                "ready for some well-deserved vacation.",
+                "Little do you know that your fight is far from over!",
+                "Your ultimate enemy is the lady from the Dean's office",
+                "the only person who can give you your Grade Transcript.",
+                "Only young, naive freshmen believe it's an easy task!",
+                "Catch the lady, be nice and if today is your lucky day",
+                "she might give you the Holy Graal of every student."]
 
 
 def create_player():
@@ -20,6 +53,7 @@ def create_player():
     player['name'] = name
     player['field'] = PLAYER_START_COORDS
     player['icon'] = PLAYER_ICON
+    player['level'] = 1
     return player
 
 
@@ -29,38 +63,68 @@ def get_npc_direction():
     return key
 
 
-def setup_start_board(board, player, npcs, items):
-    engine.put_player_on_board(board, player)
-    #engine.put_items_on_board(board, items)
-    #engine.put_npcs_on_board(board, npcs)
-    engine.put_boss_on_board(board)
+def setup_start_boards(boards, player, npcs, items):
+    for level in LEVELS:
+        items_on_level = [item for item in items if item["level"] == level]
+        print(items_on_level)
+        npcs_on_level = [npc for npc in npcs if npc["level"] == level]
+        print(npcs_on_level)
+        engine.put_items_on_board(boards[level - 1], items_on_level)
+        engine.put_npcs_on_board(boards[level - 1], npcs_on_level)
+        ui.display_board(boards[level - 1], player)
+        if level == 1:
+            engine.put_player_on_board(boards[level - 1], player)
+
+
+def initialize_game():
+    if engine.play_new_game():
+        player = create_player()
+        npcs = deepcopy(NPCS) 
+        items = deepcopy(ITEMS)
+        boards = [engine.create_board(BOARD_WIDTH, BOARD_HEIGHT, level) for level in LEVELS]
+        setup_start_boards(boards, player, npcs, items)
+    else:
+        boards, items, npcs, player = gamesaves.load_game()
+    return boards, items, npcs, player
+
+
+def react_on_key(boards, items, npcs, player, level, key):
+    if key == 'Q':
+        return False
+    elif key == 'V':
+        gamesaves.save_game(boards, items, npcs, player)
+        print("Saving game..")
+        time.sleep(2)
+        return False
+    elif key == 'I':
+        util.clear_screen()
+        ui.display_inventory(player)
+        return True
+    else:
+        engine.move(player, boards[level -1], key, player, items)
+        for npc in npcs:
+            if npc["level"] == level:
+                engine.move(npc, boards[level -1], get_npc_direction(), player, items)
+        return True
 
 
 def main():
-    player = create_player()
-    board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
-    npcs = deepcopy(NPCS) 
-    items = deepcopy(ITEMS)
-    setup_start_board(board, player, npcs, items)
-    util.clear_screen()
+    boards, items, npcs, player = initialize_game()
+    #util.clear_screen()
     is_running = True
     while is_running:
+        level = player["level"]
+        ui.display_board(boards[level - 1], player)
+        engine.interaction_with_npc(boards[level -1], player, npcs)
         if player["energy"] <= 0:
-            break
-        ui.display_board(board, player)
-        key = util.key_pressed().upper()
-        if key == 'Q':
+            print("GAME OVER")
             is_running = False
-        elif key == 'I':
-            util.clear_screen()
-            ui.display_inventory(player)
-        else:
-            engine.move(player, board, key, player, items, npcs)
-            # engine.interaction_with_item(board, player, items)
-            #for npc in npcs:                                                 # This needs to be changed, npc appear out of nowhere if we don't put them on board
-                #engine.move(npc, board, get_npc_direction(), player, items, npcs)
-            engine.move_boss(board, BOSS)
-        util.clear_screen()
+            break
+        #util.clear_screen() # uncomment in final version
+        ui.display_board(boards[level - 1], player)
+        key = util.key_pressed().upper()
+        is_running = react_on_key(boards, items, npcs, player, level, key)
+        #util.clear_screen()
 
 
 if __name__ == "__main__":
