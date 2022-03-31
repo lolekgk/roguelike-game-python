fd43777952a1259b74464ceb78ab21380551fd11
Karolina Chalupka <k.s.chalupka@gmail.com>
2022-03-31 15:07:28 +0200

    Add new line

--- ui.py
+++ ui.py
@@ -103,28 +103,28 @@ def meeting_npc(npc):
 def finding_items(item):
     message = ""
     if item["name"] == "notes":
-        message = "Cool! Somebody left their notes here."
+        message = "\nCool! Somebody left their notes here."
     elif item["name"] == "Red Bull":
-        message = "I was getting a bit sleepy. This energy drink comes right in time!"
+        message = "\nI was getting a bit sleepy. This energy drink comes right in time!"
     elif item["name"] == "instant noodles":
-        message = "My favourite instant noodles! Just as I was getting hungry!"
+        message = "\nMy favourite instant noodles! Just as I was getting hungry!"
     elif item["name"] == "beer":
-        message = "Someone left a beer in the University dorm! That's crazy! Should I drink it now... Nah, I'll keep it in my backpack. "
+        message = "\nSomeone left a beer in the University dorm! That's crazy! Should I drink it now... Nah, I'll keep it in my backpack. "
     elif item["name"] == "nerd's notes":
-        message = "Thank's man! With these notes the exam will be a breeze!"
+        message = "\nThank's man! With these notes the exam will be a breeze!"
     elif item["name"] == "last year's test":
-        message = "Dude, you're the best! Now let's hope the professor uses the exact same test this year!"
+        message = "\nDude, you're the best! Now let's hope the professor uses the exact same test this year!"
     elif item["name"] == "flowers":
-        message = "A nice bouquet of flowers! The lady from the Dean's office might like it."
+        message = "\nA nice bouquet of flowers! The lady from the Dean's office might like it."
     elif item["name"] == "chocolates":
-        message = "Fancy belgian chocolates. They will help me get some favours!"
+        message = "\nFancy belgian chocolates. They will help me get some favours!"
     elif item["name"] == "exam":
-        message = "This exam is more difficult than I expected..."
+        message = "\nThis exam is more difficult than I expected..."
     elif item["name"] == "key":
-        message = "I found a secret key!!!"
+        message = "\nI found a secret key!!!"
     else:
         pass
-    print(bcolors.BLUE, message, bcolors.ENDC)
+    print(message)
 
 
 def select_game_state():
